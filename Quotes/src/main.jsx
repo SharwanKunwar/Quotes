53f48fd1be4c721c24ace6701b15c71ceba41a81
Sharwan jung kunwar <sharwankunwar07@gmail.com>
2025-11-22 18:14:09 +0545

    updated

--- Quotes/src/main.jsx
+++ Quotes/src/main.jsx
@@ -1,55 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Inspiration from './pages/Inspiration.jsx'
-import Humor from './pages/Humor.jsx'
-import Philosopy from './pages/Philosopy.jsx'
-import Wisdom from './pages/Wisdom.jsx'
-import Love from './pages/Love.jsx'
-import Sucess from './pages/Sucess.jsx'
-import DefaultPage from './pages/DefaultPage.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Inspiration from './pages/Inspiration.jsx';
+import Humor from './pages/Humor.jsx';
+import Philosophy from './pages/Philosophy.jsx';
+import Wisdom from './pages/Wisdom.jsx';
+import Love from './pages/Love.jsx';
+import Success from './pages/Success.jsx';
+import DefaultPage from './pages/DefaultPage.jsx';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <App/>,
-    children:[
-      {
-        index: true,
-        element: <DefaultPage/>
-      },
-      {
-        path: "inspiration",
-        element: <Inspiration/>
-      },
-      {
-        path: "humor",
-        element: <Humor/>
-      },
-      {
-        path:"philosopy",
-        element: <Philosopy/>
-      },
-      {
-        path: "wisdom",
-        element: <Wisdom/>
-      },
-      {
-        path: "sucess",
-        element: <Sucess/>
-      },
-      {
-        path: "love",
-        element: <Love/>
-      }
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <DefaultPage /> },
+      { path: 'inspiration', element: <Inspiration /> },
+      { path: 'humor', element: <Humor /> },
+      { path: 'philosophy', element: <Philosophy /> },
+      { path: 'wisdom', element: <Wisdom /> },
+      { path: 'success', element: <Success /> },
+      { path: 'love', element: <Love /> }
     ]
   }
-])
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
